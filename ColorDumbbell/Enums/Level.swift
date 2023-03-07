43c762d720640e5f2ad9dc3c166e2c05061dbddc
myungsun <audtjs9987@naver.com>
2023-03-07 14:42:46 +0900

    chore: add Level enum file

---
+++ ColorDumbbell/Enums/Level.swift
@@ -0,0 +1,21 @@
+//
+//  Level.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/03/07.
+//
+
+import Foundation
+
+enum Level: String {
+    case pink = "pink"
+    case yellow = "yellow"
+    case orange = "orange"
+    case green = "green"
+    case purple = "purple"
+    case blue = "blue"
+    case brown = "brown"
+    case red = "red"
+    case gray = "gray"
+    case black = "black"
+}
