75a49a3cf1f19f9cfe3ed27221a0cf2b5ddcf57b
myungsun <audtjs9987@naver.com>
2023-03-07 14:06:10 +0900

    chore: add EditorMode enum file

---
+++ ColorDumbbell/Enums/EditorMode.swift
@@ -0,0 +1,15 @@
+//
+//  EditorMode.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/03/07.
+//
+
+import Foundation
+
+enum EditorMode {
+    case fetch
+    case new
+    case edit
+    case delete
+}
