43d2940f3b1243421fde5d16c160aa9aebd27fc4
myungsun <audtjs9987@naver.com>
2023-03-07 13:57:05 +0900

    chore: add Constants enum file

---
+++ ColorDumbbell/Enums/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/03/07.
+//
+
+import Foundation
+
+public enum Cell {
+    
+}
+
+public enum VC {
+    
+}
+
+public enum Storyboard {
+    static let main = "Main"
+}
