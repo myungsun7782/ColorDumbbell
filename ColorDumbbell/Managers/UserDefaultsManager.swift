5280df50b9008c1cce61bc1103018a4cabad1a4d
myungsun <audtjs9987@naver.com>
2023-03-07 14:03:33 +0900

    chore: make UserDefaultsManager class

---
+++ ColorDumbbell/Managers/UserDefaultsManager.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultsManager.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/03/07.
+//
+
+import UIKit
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+
+    private init() {}
+}
