779d010e38363374000d67a79d22ea6661005200
myungsun <audtjs9987@naver.com>
2023-03-09 19:46:18 +0900

    feat: develop FirebaseManager using Singleton

--- ColorDumbbell/Managers/FirebaseManager.swift
+++ ColorDumbbell/Managers/FirebaseManager.swift
@@ -13,7 +13,23 @@ class FirebaseManager {
     static let shared = FirebaseManager()
     private let db = Firestore.firestore()
     private let storage = Storage.storage()
-    
+    private let USER_COLLECTION: String = "users"
     private init() {}
     
+    func addUser(user: User, completionHandler: @escaping (_ isSuccess: Bool) -> ()) {
+        db.collection(USER_COLLECTION).addDocument(data: [
+            UserCodingKeys.uid.rawValue: user.uid,
+            UserCodingKeys.name.rawValue: user.name,
+            UserCodingKeys.exerciseTime.rawValue: Timestamp(date: user.exerciseTime),
+            UserCodingKeys.currentLevel.rawValue: user.currentLevel,
+            UserCodingKeys.totalExerciseCount.rawValue: user.totalExerciseCount
+        ]) { (err) in
+            if let err = err {
+                print("Error adding document: \(err)")
+                completionHandler(false)
+            } else {
+                completionHandler(true)
+            }
+        }
+    }
 }
