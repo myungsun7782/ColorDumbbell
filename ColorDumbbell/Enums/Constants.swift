c037d6ac4ea46a3fd20de52ef6fa9c61611933fb
myungsun <audtjs9987@naver.com>
2023-03-09 19:44:38 +0900

    chore: add enum VC properties

--- ColorDumbbell/Enums/Constants.swift
+++ ColorDumbbell/Enums/Constants.swift
@@ -12,8 +12,10 @@ public enum Cell {
 }
 
 public enum VC {
+    static let mainVC = "MainVC"
     static let usernameVC = "UsernameVC"
     static let exerciseTimeVC = "ExerciseTimeVC"
+    static let mainTabBarController = "MainTabBarController"
 }
 
 public enum Storyboard {
