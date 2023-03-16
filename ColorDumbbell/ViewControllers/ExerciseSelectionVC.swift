7752fe3ea8faa659ab01b9bca238eeee2c6385c0
myungsun <audtjs9987@naver.com>
2023-03-16 21:24:16 +0900

    feat: develop ExerciseSelection view controller

--- ColorDumbbell/ViewControllers/ExerciseSelectionVC.swift
+++ ColorDumbbell/ViewControllers/ExerciseSelectionVC.swift
@@ -6,24 +6,244 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxGesture
 
 class ExerciseSelectionVC: UIViewController {
-
+    // UITableView
+    @IBOutlet weak var selectionTableView: UITableView!
+    
+    // UIButton
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    
+    // ViewModel
+    let viewModel = ExerciseSelectionVM()
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+    
+    // Constants
+    let ACTION_SHEET_ALERT_MESSAGE: String = "이 변경 사항을 폐기하겠습니까?"
+    let ALERT_DISCARD_ACTION_TITLE: String = "변경 사항 폐기"
+    let ALERT_CANCEL_ACTION_TITLE: String = "계속 편집하기"
+    let NUMBER_OF_SECTION: Int = 6
+    let BUTTON_FONT_SIZE: CGFloat = 17
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initUI()
+        action()
+        bind()
+        self.presentationController?.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        isModalInPresentation = false
     }
-    */
+    
+    override func viewWillLayoutSubviews() {
+        selectionTableView.reloadData()
+    }
 
+    private func initUI() {
+        // UITableView
+        configureTableView()
+        
+        // UIButton
+        configureButton()
+    }
+    
+    private func action() {
+        // UIButton
+        cancelButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.presentActionSheetAlert()
+            })
+            .disposed(by: disposeBag)
+        
+        finishButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.viewModel.exerciseDelegate?.selectExercises(exerciseArray:  self.viewModel.getSelectedExercises(totalExerciseArray: self.viewModel.totalExerciseArray))
+                self.dismiss(animated: true)
+               
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func bind() {
+        
+    }
+    
+    private func configureTableView() {
+        selectionTableView.dataSource = self
+        selectionTableView.delegate = self
+        
+        selectionTableView.register(UINib(nibName: Cell.exerciseTitleCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseTitleCell)
+        selectionTableView.register(UINib(nibName: Cell.exerciseContentCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseContentCell)
+        selectionTableView.register(UINib(nibName: Cell.exerciseRegisterCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseRegisterCell)
+    }
+    
+    private func configureButton() {
+        finishButton.isEnabled = viewModel.validateFinishButton()
+        finishButton.setTitleColor(finishButton.isEnabled ? ColorManager.shared.getCarminePink() : ColorManager.shared.getQuickSilver(), for: .normal)
+    }
+    
+    private func presentActionSheetAlert() {
+        let alert = UIAlertController(title: nil, message: ACTION_SHEET_ALERT_MESSAGE, preferredStyle: .actionSheet)
+        
+        let discardAction = UIAlertAction(title: ALERT_DISCARD_ACTION_TITLE, style: .destructive) { _ in
+            self.dismiss(animated: true)
+        }
+        let cancelAction = UIAlertAction(title: ALERT_CANCEL_ACTION_TITLE, style: .cancel)
+        alert.addAction(discardAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func presentAddExerciseVC(editorMode: EditorMode, section: Int, row: Int, exerciseName: String? = nil) {
+        let addExerciseVC = UIStoryboard(name: Storyboard.main, bundle: nil).instantiateViewController(withIdentifier: VC.addExerciseVC) as! AddExerciseVC
+        
+        addExerciseVC.viewModel.editorMode = editorMode
+        addExerciseVC.viewModel.exerciseName = exerciseName
+        addExerciseVC.viewModel.section = section
+        addExerciseVC.viewModel.row = row
+        addExerciseVC.viewModel.exerciseDelegate = self
+        addExerciseVC.modalPresentationStyle = .overCurrentContext
+        addExerciseVC.modalTransitionStyle = .coverVertical
+        
+        self.present(addExerciseVC, animated: true)
+    }
+}
+
+extension ExerciseSelectionVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return NUMBER_OF_SECTION
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[0].count + 2 : viewModel.totalExerciseArray[0].count + 1
+        } else if section == 1 {
+            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[1].count + 2 : viewModel.totalExerciseArray[1].count + 1
+        } else if section == 2 {
+            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[2].count + 2 : viewModel.totalExerciseArray[2].count + 1
+        } else if section == 3 {
+            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[3].count + 2 : viewModel.totalExerciseArray[3].count + 1
+        } else if section == 4 {
+            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[4].count + 2 : viewModel.totalExerciseArray[4].count + 1
+        }
+        
+        return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[5].count + 2 : viewModel.totalExerciseArray[5].count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 || indexPath.section == 1 || indexPath.section == 2 || indexPath.section == 3 || indexPath.section == 4 || indexPath.section == 5 {
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseTitleCell) as! ExerciseTitleCell
+                cell.setData(index: indexPath.section, isEditorModeOn: viewModel.isEditorModeOn)
+                
+                cell.editButton.rx.tap
+                    .subscribe(onNext: { _ in
+                        self.viewModel.isEditorModeOn = !self.viewModel.isEditorModeOn
+                        DispatchQueue.main.async {
+                            self.selectionTableView.reloadData()
+                            self.view.layoutIfNeeded()
+                        }
+                    })
+                    .disposed(by: cell.disposeBag)
+                
+                return cell
+            }
+            if !viewModel.isEditorModeOn {
+                if indexPath.row != 0 {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
+                    
+                    cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name, isEditorModeOn: false, index: indexPath.row, exerciseArray: viewModel.totalExerciseArray[indexPath.section])
+                    cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
+                    cell.containerStackView.rx.tapGesture()
+                        .when(.recognized)
+                        .subscribe(onNext: { _ in
+                            self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked = !self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked
+                            DispatchQueue.main.async {
+                                self.selectionTableView.reloadData()
+                            }
+                            self.configureButton()
+                        })
+                        .disposed(by: cell.disposeBag)
+                    
+                    return cell
+                }
+            } else {
+                if indexPath.row == viewModel.totalExerciseArray[indexPath.section].count + 1 {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseRegisterCell) as! ExerciseRegisterCell
+                    
+                    cell.setData(isEditorModeOn: viewModel.isEditorModeOn)
+                    cell.containerStackView.rx.tapGesture()
+                        .when(.recognized)
+                        .subscribe(onNext: { _ in
+                            self.presentAddExerciseVC(editorMode: .new, section: indexPath.section, row: indexPath.row-1)
+                        })
+                        .disposed(by: cell.disposeBag)
+                    
+                    return cell
+                } else {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
+                    
+                    cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name, isEditorModeOn: true, index: indexPath.row, exerciseArray: viewModel.totalExerciseArray[indexPath.section])
+                    cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
+                    cell.containerStackView.rx.tapGesture()
+                        .when(.recognized)
+                        .subscribe(onNext: { _ in
+                            self.presentAddExerciseVC(editorMode: .edit, section: indexPath.section, row: indexPath.row-1, exerciseName: self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name)
+                            
+                            DispatchQueue.main.async {
+                                self.selectionTableView.reloadData()
+                            }
+                            self.configureButton()
+                        })
+                        .disposed(by: cell.disposeBag)
+                    
+                    cell.deleteButton.rx.tap
+                        .subscribe(onNext: { _ in
+                            AlertManager.shared.presentTwoButtonAlert(title: "운동 삭제", message: "정말로 해당 운동을 삭제하시겠습니까?", buttonTitle: "확인", style: .alert) {
+                                // MARK: - Cloud Firestore에서도 삭제하기
+                                self.viewModel.totalExerciseArray[indexPath.section].remove(at: indexPath.row-1)
+                                DispatchQueue.main.async {
+                                    self.selectionTableView.reloadData()
+                                }
+                            } completionHandler: { alert in
+                                self.present(alert, animated: true)
+                            }
+                        })
+                        .disposed(by: cell.disposeBag)
+                    
+                    return cell
+                }
+            }
+        }
+        return UITableViewCell()
+    }
+}
+
+extension ExerciseSelectionVC: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+        presentActionSheetAlert()
+        return false
+    }
+}
+
+extension ExerciseSelectionVC: ExerciseDelegate {
+    func selectExercises(exerciseArray: [Exercise]) {}
+    func manageExercise(section: Int, row: Int, exerciseName: String, editorMode: EditorMode) {
+        if editorMode == .new {
+            viewModel.totalExerciseArray[section].append(Exercise(name: exerciseName, area: viewModel.EXERCISE_AREA_ARRAY[section].rawValue, quantity: [ExerciseQuantity]()))
+        } else if editorMode == .edit {
+            viewModel.totalExerciseArray[section][row].name = exerciseName
+        }
+        selectionTableView.reloadData()
+    }
 }
