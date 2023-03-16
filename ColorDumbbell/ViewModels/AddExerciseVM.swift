162513083b6b1379ea28a31c29bd53077e992ff5
myungsun <audtjs9987@naver.com>
2023-03-16 21:26:14 +0900

    feat: develop AddExercise view model

--- ColorDumbbell/ViewModels/AddExerciseVM.swift
+++ ColorDumbbell/ViewModels/AddExerciseVM.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+
+class AddExerciseVM {
+    // Input
+    var input = Input()
+    
+    // Output
+    var output = Output()
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+    
+    // Variable
+    var editorMode: EditorMode = .new
+    var exerciseName: String?
+    var exerciseDelegate: ExerciseDelegate?
+    var section: Int!
+    var row: Int!
+    
+    // Constants
+    let MINIMUM_LENGTH: Int = 2
+    let MAXIMUM_LENGTH: Int = 6
+    
+    struct Input {
+        var exerciseName = BehaviorSubject<String>(value: "")
+    }
+    
+    struct Output {
+        var saveButtonValidation = PublishSubject<Bool>()
+    }
+    
+    init() {
+        input.exerciseName.subscribe(onNext: { exerciseName in
+            self.output.saveButtonValidation.onNext(self.validateSaveButton(exerciseName: exerciseName))
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func validateSaveButton(exerciseName: String) -> Bool {
+        if exerciseName.count >= 1 {
+            return true
+        }
+        return false
+    }
+
+}
