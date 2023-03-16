ca60cf65016f904420791d2e5dac759d27ff842c
myungsun <audtjs9987@naver.com>
2023-03-16 21:17:19 +0900

    add enum properties

--- ColorDumbbell/Enums/Constants.swift
+++ ColorDumbbell/Enums/Constants.swift
@@ -8,14 +8,26 @@
 import Foundation
 
 public enum Cell {
-    
+    static let titleCell = "TitleCell"
+    static let exerciseTimeCell = "ExerciseTimeCell"
+    static let addCell = "AddCell"
+    static let photoFileCell = "PhotoFileCell"
+    static let exerciseAddCell = "ExerciseAddCell"
+    static let exerciseTitleCell = "ExerciseTitleCell"
+    static let exerciseContentCell = "ExerciseContentCell"
+    static let exerciseRegisterCell = "ExerciseRegisterCell"
+    static let exerciseSetCell = "ExerciseSetCell"
+    static let exerciseUtilityCell = "ExerciseUtilityCell"
 }
 
 public enum VC {
     static let mainVC = "MainVC"
     static let usernameVC = "UsernameVC"
     static let exerciseTimeVC = "ExerciseTimeVC"
+    static let journalRegisterVC = "JournalRegisterVC"
+    static let exerciseSelectionVC = "ExerciseSelectionVC"
     static let mainTabBarController = "MainTabBarController"
+    static let addExerciseVC = "AddExerciseVC"
 }
 
 public enum Storyboard {
