e4005ae6fcdd616aaac87065ea272e7b4f29bf9b
myungsun <audtjs9987@naver.com>
2023-03-16 21:27:45 +0900

    chore: add ExerciseQuantity DTO model

--- ColorDumbbell/Models/ExerciseQuantity.swift
+++ ColorDumbbell/Models/ExerciseQuantity.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct ExerciseQuantity {
+    var weight: Double // 무게(kg)
+    var reps: Int
+    
+    init() {
+        self.weight = 0
+        self.reps = 0
+    }
+    
+    init(weight: Double, reps: Int) {
+        self.weight = weight
+        self.reps = reps
+    }
+}
