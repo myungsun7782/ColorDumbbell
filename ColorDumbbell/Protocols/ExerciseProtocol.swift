1d554beb4760f8326ccd1d6af6c2cb0b1d7e07df
myungsun <audtjs9987@naver.com>
2023-03-16 21:15:47 +0900

    feat: define ExerciseProtocol

--- ColorDumbbell/Protocols/ExerciseProtocol.swift
+++ ColorDumbbell/Protocols/ExerciseProtocol.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+protocol ExerciseDelegate: AnyObject {
+    func manageExercise(section: Int, row: Int, exerciseName: String, editorMode: EditorMode)
+    func selectExercises(exerciseArray: [Exercise])
+}
