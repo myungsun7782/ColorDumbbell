7d964bc09c6d9b2947d61f27c378d7219cdb1dac
myungsun <audtjs9987@naver.com>
2023-03-16 21:20:50 +0900

    feat: develop TitleCell

--- ColorDumbbell/Cells/UITableViewCell/TitleCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/TitleCell.swift
@@ -8,16 +8,34 @@
 import UIKit
 
 class TitleCell: UITableViewCell {
-
+    // UITextField
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    // UIStackView
+    @IBOutlet weak var containerStackView: UIStackView!
+    
+    // Constants
+    let TEXT_FIELD_PLACE_HOLDER: String = "제목"
+    let TEXT_FIELD_FONT_SIZE: CGFloat = 17
+    let STACK_VIEW_CORNER_RADIUS: CGFloat = 7
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        initUI()
     }
     
+    private func initUI() {
+        // UIStackView
+        containerStackView.layer.cornerRadius = STACK_VIEW_CORNER_RADIUS
+        
+        // UITextField
+        configureTextField()
+    }
+    
+    private func configureTextField() {
+        titleTextField.font = FontManager.shared.getPretendardRegular(fontSize: TEXT_FIELD_FONT_SIZE)
+        titleTextField.attributedPlaceholder = NSAttributedString(string: TEXT_FIELD_PLACE_HOLDER, attributes: [NSAttributedString.Key.foregroundColor : ColorManager.shared.getSilverSand()])
+        titleTextField.addLeftPadding()
+    }
 }
