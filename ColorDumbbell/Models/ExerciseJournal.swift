b1b89094b9de890928b572a25c015037598f113b
myungsun <audtjs9987@naver.com>
2023-03-16 21:28:07 +0900

    chore: add ExerciseJournal DTO model

--- ColorDumbbell/Models/ExerciseJournal.swift
+++ ColorDumbbell/Models/ExerciseJournal.swift
@@ -5,4 +5,14 @@
 //  Created by myungsun on 2023/03/11.
 //
 
-import Foundation
+import UIKit
+
+struct ExerciseJournal {
+    var id: String
+    var registerDate: Date
+    var startTime: Date
+    var endTime: Date
+    var totalExerciseTime: Int
+    var photoIdArray: [String]?
+    var exerciseArray: [Exercise]
+}
