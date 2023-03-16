b29f95c000649b82089bfce91e6f3a992d478a5e
myungsun <audtjs9987@naver.com>
2023-03-16 21:24:52 +0900

    feat: develop AddExercise view controller

--- ColorDumbbell/ViewControllers/AddExerciseVC.swift
+++ ColorDumbbell/ViewControllers/AddExerciseVC.swift
@@ -6,24 +6,136 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class AddExerciseVC: UIViewController {
-
+    // UIView
+    @IBOutlet weak var containerView: UIView!
+    
+    // UIStackView
+    @IBOutlet weak var popUpStackView: UIStackView!
+    
+    // UITextField
+    @IBOutlet weak var exerciseTextField: UITextField!
+    
+    // UIButton
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    // UILabel
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // ViewModel
+    let viewModel = AddExerciseVM()
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+    
+    // Constants
+    let STACK_VIEW_BORDER_RADIUS: CGFloat = 7
+    let TEXT_FIELD_CORNER_RADIUS: CGFloat = 5
+    let TEXT_FIELD_FONT_SIZE: CGFloat = 16
+    let TEXT_FIELD_BORDER_WIDTH: CGFloat = 1
+    let TEXT_FIELD_PLACE_HODLER: String = "운동의 이름은 1글자 이상이어야 합니다!"
+    let ADD_TITLE_TEXT: String = "새로운 운동 추가"
+    let ADD_DESCRIPTION_TEXT: String = "추가할 운동을 입력해주세요 :)"
+    let MODIFICATION_TITLE_TEXT: String = "기존 운동 수정"
+    let MODIFICATION_DESCRIPTION_TEXT: String = "수정할 운동 이름을 입력해주세요 :)"
+    let BUTTON_CORNER_RADIUS: CGFloat = 7
+    let BUTTON_BORDER_WIDTH: CGFloat = 1
+    let CONTAINER_VIEW_ALPHA: CGFloat = 0.65
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initUI()
+        action()
+        bind()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        exerciseTextField.becomeFirstResponder()
     }
-    */
-
+    
+    private func initUI() {
+        // UIStackView
+        popUpStackView.layer.cornerRadius = STACK_VIEW_BORDER_RADIUS
+        
+        // UIView
+        containerView.backgroundColor = .black
+        containerView.alpha = CONTAINER_VIEW_ALPHA
+        
+        // UITextField
+        configureTextField()
+        
+        // UIButton
+        configureButton()
+        
+        // UILabel
+        configureLabel()
+    }
+    
+    private func action() {
+        // UIButton
+        cancelButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        saveButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.viewModel.exerciseDelegate?.manageExercise(section: self.viewModel.section, row: self.viewModel.row, exerciseName: self.exerciseTextField.text!, editorMode: self.viewModel.editorMode)
+                self.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func bind() {
+        // Input
+        exerciseTextField.rx.text.orEmpty
+            .bind(to: viewModel.input.exerciseName)
+            .disposed(by: disposeBag)
+        
+        // Output
+        viewModel.output.saveButtonValidation
+            .subscribe(onNext: { isValidated in
+                self.saveButton.isEnabled = isValidated
+                self.saveButton.setTitleColor(self.saveButton.isEnabled ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getArgent(), for: .normal)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureLabel() {
+        titleLabel.text = viewModel.editorMode == .new ? ADD_TITLE_TEXT : MODIFICATION_TITLE_TEXT
+        descriptionLabel.text = viewModel.editorMode == .new ? ADD_DESCRIPTION_TEXT : MODIFICATION_DESCRIPTION_TEXT
+    }
+    
+    
+    private func configureButton() {
+        cancelButton.layer.addBorder([.top,.right], color: ColorManager.shared.getGrayX11(), width: BUTTON_BORDER_WIDTH)
+        cancelButton.roundCorners(corners: [.bottomLeft], radius: BUTTON_CORNER_RADIUS)
+        
+        saveButton.layer.addBorder([.top], color: ColorManager.shared.getGrayX11(), width: BUTTON_BORDER_WIDTH)
+        saveButton.roundCorners(corners: [.bottomRight], radius: BUTTON_CORNER_RADIUS)
+        saveButton.isEnabled = viewModel.editorMode == .new ? false : true
+        self.saveButton.setTitleColor(self.saveButton.isEnabled ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getArgent(), for: .normal)
+    }
+    
+    private func configureTextField() {
+        exerciseTextField.font = FontManager.shared.getPretendardRegular(fontSize: TEXT_FIELD_FONT_SIZE)
+        exerciseTextField.layer.borderWidth = TEXT_FIELD_BORDER_WIDTH
+        exerciseTextField.layer.borderColor = ColorManager.shared.getGrayX11().cgColor
+        exerciseTextField.layer.cornerRadius = TEXT_FIELD_CORNER_RADIUS
+        exerciseTextField.addLeftPadding()
+        exerciseTextField.attributedPlaceholder = NSAttributedString(string: TEXT_FIELD_PLACE_HODLER, attributes: [NSAttributedString.Key.foregroundColor : ColorManager.shared.getLavenderGray()])
+        
+        if let exerciseName = viewModel.exerciseName {
+            exerciseTextField.text = exerciseName
+            viewModel.input.exerciseName.onNext(exerciseName)
+        }
+    }
 }
+
