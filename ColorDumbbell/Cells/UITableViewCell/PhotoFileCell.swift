3290f727968419d2fbdf1def07f89a432dd2ab21
myungsun <audtjs9987@naver.com>
2023-03-16 21:29:09 +0900

    feat: develop PhotoFileCell

--- ColorDumbbell/Cells/UITableViewCell/PhotoFileCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/PhotoFileCell.swift
@@ -6,18 +6,54 @@
 //
 
 import UIKit
+import RxSwift
 
 class PhotoFileCell: UITableViewCell {
-
+    // UIStackView
+    @IBOutlet weak var containerStackView: UIStackView!
+    
+    // UILabel
+    @IBOutlet weak var photoIdLabel: UILabel!
+    
+    // NSLayoutConstarint
+    @IBOutlet weak var topConst: NSLayoutConstraint!
+    
+    // Variables
+    var index: Int?
+    
+    // RxSwift
+    var disposeBag = DisposeBag()
+    
+    // Constants
+    let CORNER_RADIUS: CGFloat = 7
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+  
+    
+    override func setNeedsLayout() {
+        if let index = index {
+            if index == 0 {
+                containerStackView.layer.cornerRadius = 0
+                containerStackView.roundCorners(corners: [.topLeft, .topRight], radius: CORNER_RADIUS)
+            } else {
+                containerStackView.roundCorners(corners: .allCorners, radius: 0)
+            }
+            self.layoutIfNeeded()
+        }
     }
     
+    func setData(index: Int, photoId: String) {
+        self.index = index
+        topConst.constant = index == 0 ? 18 : 0
+        containerStackView.layer.cornerRadius = 0
+        photoIdLabel.text = photoId
+    }
 }
