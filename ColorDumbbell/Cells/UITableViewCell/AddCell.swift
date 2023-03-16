da2012ab21e5c6394fbcc56e033085c32911e204
myungsun <audtjs9987@naver.com>
2023-03-16 21:22:29 +0900

    feat: develop AddCell

--- ColorDumbbell/Cells/UITableViewCell/AddCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/AddCell.swift
@@ -6,18 +6,62 @@
 //
 
 import UIKit
+import RxSwift
 
 class AddCell: UITableViewCell {
+    // UIStackView
+    @IBOutlet weak var containerStackView: UIStackView!
+    
+    // UILabel
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // NSLayoutConstraint
+    @IBOutlet weak var topConst: NSLayoutConstraint!
 
+    // RxSwift
+    var disposeBag = DisposeBag()
+    
+    // Variables
+    var isEmpty: Bool?
+
+    // Constants
+    let STACK_VIEW_CORNER_RADIUS: CGFloat = 7
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        initUI()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if let isEmpty = isEmpty {
+            if isEmpty {
+                containerStackView.layer.cornerRadius = 7
+            } else {
+                containerStackView.layer.cornerRadius = 0
+                containerStackView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 7)
+            }
+        }
+    }
+    
+    private func initUI() {
+        // UIStackView
+        containerStackView.layer.cornerRadius = STACK_VIEW_CORNER_RADIUS
+    }
+    
+    func setData(title: String) {
+        titleLabel.text = title
+        
+    }
+    
+    func setTopConstarint(isEmpty: Bool) {
+        self.isEmpty = isEmpty
+        topConst.constant = isEmpty ? 18 : 0
+    }
 }
