cda8c585e5af20139a078e9325ceecdb76770c23
myungsun <audtjs9987@naver.com>
2023-03-16 21:18:23 +0900

    feat: develop AlertManager class

--- ColorDumbbell/Managers/AlertManager.swift
+++ ColorDumbbell/Managers/AlertManager.swift
@@ -5,4 +5,34 @@
 //  Created by myungsun on 2023/03/15.
 //
 
-import Foundation
+import UIKit
+
+class AlertManager {
+    static let shared = AlertManager()
+    let CONFIRM_BUTTON_TITLE: String = "확인"
+    let CANCEL_BUTTON_TITLE: String = "취소"
+    
+    private init() {}
+    
+    func presentOneButtonAlert(title: String, message: String, buttonHandler: @escaping () -> (), completionHandler: @escaping (_ alert: UIAlertController) -> ()) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let button = UIAlertAction(title: CONFIRM_BUTTON_TITLE, style: .default) { _ in
+            buttonHandler()
+        }
+        alert.addAction(button)
+        completionHandler(alert)
+    }
+    
+    func presentTwoButtonAlert(title: String, message: String, buttonTitle: String, style: UIAlertController.Style, buttonHandler: @escaping () -> (), completionHandler: @escaping (_ alert: UIAlertController) -> ()) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let rightButton = UIAlertAction(title: buttonTitle, style: .destructive) { _ in
+            buttonHandler()
+        }
+        let cancelButton = UIAlertAction(title: CANCEL_BUTTON_TITLE, style: .cancel)
+        
+        alert.addAction(rightButton)
+        alert.addAction(cancelButton)
+        completionHandler(alert)
+    }
+}
+
