98698e9fc0f6ea2014f82e92859d79acf971d1d8
myungsun <audtjs9987@naver.com>
2023-03-16 21:30:52 +0900

    feat: develop ExerciseTitle cell

--- ColorDumbbell/Cells/UITableViewCell/ExerciseTitleCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/ExerciseTitleCell.swift
@@ -6,18 +6,114 @@
 //
 
 import UIKit
+import RxSwift
 
 class ExerciseTitleCell: UITableViewCell {
-
+    // UIStackView
+    @IBOutlet weak var containerStackView: UIStackView!
+    
+    // UILabel
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // UIButton
+    @IBOutlet weak var editButton: UIButton!
+    
+    // NSLayoutConstraint
+    @IBOutlet weak var topConst: NSLayoutConstraint!
+    
+    // RxSwift
+    var disposeBag = DisposeBag()
+    
+    // Variable
+    var index: Int?
+    
+    // Constants
+    let STACK_VIEW_BORDER_RADIUS: CGFloat = 7
+    let BUTTON_FONT_SIZE: CGFloat = 16
+    let LABEL_FONT_SIZE: CGFloat = 17
+    let TOP_CONST: CGFloat = 11
+    let FINISH_TEXT = "완료"
+    let EDIT_TEXT = "편집"
+    let BACK_TEXT = "등 (Back)"
+    let CHEST_TEXT = "가슴 (Chest)"
+    let SHOULDER_TEXT = "어깨 (Shoulder)"
+    let LEG_TEXT = "하체 (Leg)"
+    let ARM_TEXT = "팔 (Arm)"
+    let ABS_TEXT = "복근 (Abs)"
+    let BUTTON_IMAGE: UIImage? = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 17))
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        initUI()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    override func setNeedsLayout() {
+        if index != nil {
+            containerStackView.layer.cornerRadius = .zero
+            containerStackView.roundCorners(corners: [.topLeft, .topRight], radius: STACK_VIEW_BORDER_RADIUS)
+            self.layoutIfNeeded()
+        }
+    }
+    
+    private func initUI() {
+        // UIButton
+        configureButton()
+    }
+    
+    private func configureButton() {
+        editButton.titleLabel?.font = FontManager.shared.getPretendardRegular(fontSize: BUTTON_FONT_SIZE)
+    
+    }
+    
+    func setData(index: Int, isEditorModeOn: Bool) {
+        editButton.titleLabel?.text = isEditorModeOn ? FINISH_TEXT : EDIT_TEXT
+        if isEditorModeOn {
+            editButton.setTitle(FINISH_TEXT, for: .normal)
+        } else {
+            editButton.setTitle(EDIT_TEXT, for: .normal)
+        }
+        
+        if index != 0 {
+            topConst.constant = TOP_CONST
+        } else {
+            topConst.constant = .zero
+        }
+        
+        switch index {
+        case 0:
+            titleLabel.text = BACK_TEXT
+        case 1:
+            titleLabel.text = CHEST_TEXT
+        case 2:
+            titleLabel.text = SHOULDER_TEXT
+        case 3:
+            titleLabel.text = LEG_TEXT
+        case 4:
+            titleLabel.text = ARM_TEXT
+        case 5:
+            titleLabel.text = ABS_TEXT
+        default:
+            break
+        }
+    }
+    
+    func setData(index: Int, exercise: Exercise) {
+        self.index = index
+        if index != 0 {
+            topConst.constant = TOP_CONST
+        } else {
+            topConst.constant = 18
+        }
+        
+        titleLabel.font = FontManager.shared.getPretendardBold(fontSize: BUTTON_FONT_SIZE)
+        titleLabel.text = exercise.name + " (\(exercise.area))"
+        editButton.setTitle("", for: .normal)
+        editButton.setImage(BUTTON_IMAGE, for: .normal)
+    }
 }
