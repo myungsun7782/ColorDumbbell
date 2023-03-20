7c0edba97c94b796c97fa7bbdc9108e85f7086d5
myungsun <audtjs9987@naver.com>
2023-03-20 21:09:55 +0900

    feat: develop DetailImage view controller

--- ColorDumbbell/ViewControllers/DetailImageVC.swift
+++ ColorDumbbell/ViewControllers/DetailImageVC.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DetailImageVC: UIViewController {
-
+    // UIView
+    @IBOutlet weak var containerView: UIView!
+    
+    // UIImageView
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    // UIButton
+    @IBOutlet weak var closeButton: UIButton!
+    
+    // UIImage
+    var selectedImage: UIImage?
+    
+    // RxSwift
+    let disposeBag = DisposeBag()
+    
+    // Variables
+    var isPinching = false
+    
+    // Constants
+    let BUTTON_IMAGE: UIImage? = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 23))
+    let PINCH_GESTURE_SCALE: CGFloat = 1.0
+    let ANIMATION_DURATION: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initUI()
+        action()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initUI() {
+        // UIView
+        configureView()
+        
+        // UIImageView
+        configureImageView()
+        
+        // UIButton
+        configureButton()
     }
-    */
-
+    
+    private func action() {
+        closeButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureView() {
+        // MARK: - alpha값 설정
+//        containerView.alpha = 0.65
+    }
+    
+    private func configureImageView() {
+        guard let selectedImage = selectedImage else { return }
+        detailImageView.image = selectedImage
+        
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(pinchGesture:)))
+        detailImageView.isUserInteractionEnabled = true
+        detailImageView.addGestureRecognizer(pinchGestureRecognizer)
+    }
+    
+    @objc func handlePinch(pinchGesture: UIPinchGestureRecognizer) {
+        if pinchGesture.state == .began || pinchGesture.state == .changed {
+            detailImageView.transform = detailImageView.transform.scaledBy(x: pinchGesture.scale, y: pinchGesture.scale)
+            pinchGesture.scale = PINCH_GESTURE_SCALE
+        } else if pinchGesture.state == .ended {
+            UIView.animate(withDuration: ANIMATION_DURATION) {
+                self.detailImageView.transform = CGAffineTransform.identity
+            }
+        }
+    }
+    
+    private func configureButton() {
+        closeButton.setImage(BUTTON_IMAGE, for: .normal)
+    }
 }
