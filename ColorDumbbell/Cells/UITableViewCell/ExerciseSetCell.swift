a9adfb567e42f8a8c28e6dd6da103d1b0829997e
myungsun <audtjs9987@naver.com>
2023-03-20 21:07:28 +0900

    feat: add specific logic in ExerciseSetCell

--- ColorDumbbell/Cells/UITableViewCell/ExerciseSetCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/ExerciseSetCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class ExerciseSetCell: UITableViewCell {
     // UIStackView
@@ -16,13 +17,30 @@ class ExerciseSetCell: UITableViewCell {
     @IBOutlet weak var setLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var repsLabel: UILabel!
+    @IBOutlet weak var weightDescriptionLabel: UILabel!
+    @IBOutlet weak var repsDescriptionLabel: UILabel!
+    
+    // RxSwift
+    var disposeBag = DisposeBag()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-    func setData(set: Int) {
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    func setData(set: Int, weight: Double, reps: Int, isFocused: Bool) {
         setLabel.text = "\(set)세트"
+        weightLabel.text = "\(weight)"
+        repsLabel.text = "\(reps)"
+        setLabel.textColor = isFocused ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getBlack()
+        weightLabel.textColor = isFocused ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getBlack()
+        weightDescriptionLabel.textColor = isFocused ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getBlack()
+        repsLabel.textColor = isFocused ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getBlack()
+        repsDescriptionLabel.textColor = isFocused ? ColorManager.shared.getBleuDeFrance() : ColorManager.shared.getBlack()
     }
 }
