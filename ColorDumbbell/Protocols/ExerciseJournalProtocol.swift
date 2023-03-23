caa4e61442325041d8881df6ed54ca681d8d0e1a
myungsun <audtjs9987@naver.com>
2023-03-23 20:35:02 +0900

    feat: add ExerciseJournal protocol

---
+++ ColorDumbbell/Protocols/ExerciseJournalProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ExerciseJournalProtocol.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/03/22.
+//
+
+import Foundation
+
+protocol ExerciseJournalDelegate: AnyObject {
+    func transferData(exerciseJournal: ExerciseJournal)
+}
