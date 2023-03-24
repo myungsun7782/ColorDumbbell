140c8c7f2ecf6e02fd4f628aaee62c4268eca564
myungsun <audtjs9987@naver.com>
2023-03-24 18:20:39 +0900

    feat: compose LabelCell

--- ColorDumbbell/Cells/UITableViewCell/LabelCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/LabelCell.swift
@@ -8,16 +8,11 @@
 import UIKit
 
 class LabelCell: UITableViewCell {
-
+    // NSLayoutConstarint
+    @IBOutlet weak var bottomConst: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
