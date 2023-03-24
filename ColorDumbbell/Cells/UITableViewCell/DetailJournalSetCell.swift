e8ecc320a1ea54c58bc7cedd6dfe8af5314018eb
myungsun <audtjs9987@naver.com>
2023-03-24 18:23:19 +0900

    feat: compose DetailJournalSetCell logic

--- ColorDumbbell/Cells/UITableViewCell/DetailJournalSetCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/DetailJournalSetCell.swift
@@ -6,18 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class DetailJournalSetCell: UITableViewCell {
-
+    // NSConstraintLayout
+    @IBOutlet weak var topConst: NSLayoutConstraint!
+    @IBOutlet weak var bottomConst: NSLayoutConstraint!
+    
+    // UIView
+    @IBOutlet weak var pointView: UIView!
+    
+    // UILabel
+    @IBOutlet weak var setLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var repsLabel: UILabel!
+    
+    // Constants
+    let TOP_CONSTRAINT: CGFloat = 16
+    let BOTTOM_CONSTARINT: CGFloat = 30
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
     
+    func setData(index: Int, lastIndex: Int, currentLevelColor: UIColor, set: Int, weight: Double, reps: Int) {
+        pointView.backgroundColor = currentLevelColor
+        setLabel.text = "\(set)세트"
+        weightLabel.text = "\(weight)"
+        repsLabel.text = "\(reps)"
+        pointView.layer.cornerRadius = 7.5
+        if index == lastIndex {
+            bottomConst.constant = BOTTOM_CONSTARINT
+        }
+        
+        if index == 0 {
+            topConst.constant = TOP_CONSTRAINT
+        }
+    }
 }
