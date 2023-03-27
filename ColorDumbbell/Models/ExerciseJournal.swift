7ad388728ef879d2afc0cba021d0c782c65fbea0
myungsun <audtjs9987@naver.com>
2023-03-27 20:31:22 +0900

    chore: add properties in ExerciseJournal DTO Model

--- ColorDumbbell/Models/ExerciseJournal.swift
+++ ColorDumbbell/Models/ExerciseJournal.swift
@@ -9,10 +9,12 @@ import UIKit
 
 struct ExerciseJournal {
     var id: String
+    var title: String
     var registerDate: Date
     var startTime: Date
     var endTime: Date
     var totalExerciseTime: Int
     var photoIdArray: [String]?
     var exerciseArray: [Exercise]
+    var groupedExerciseArray: [[Exercise]]?
 }
