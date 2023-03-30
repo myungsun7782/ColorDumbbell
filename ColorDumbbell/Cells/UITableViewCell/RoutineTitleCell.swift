1f9d48a10ecb5ff0d6ac1049c9d40920216879ae
myungsun <audtjs9987@naver.com>
2023-03-30 20:16:46 +0900

    feat: develop RoutineTitleCell

--- ColorDumbbell/Cells/UITableViewCell/RoutineTitleCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/RoutineTitleCell.swift
@@ -6,18 +6,44 @@
 //
 
 import UIKit
+import RxSwift
 
 class RoutineTitleCell: UITableViewCell {
-
+    // UILabel
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // UIButton
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    // NSLayoutConstraint
+    @IBOutlet weak var topConst: NSLayoutConstraint!
+    
+    // Variables
+    var disposeBag = DisposeBag()
+    
+    // Constants
+    let PARTIAL_TOP_CONST: CGFloat = 36
+    let BUTTON_IMAGE: UIImage? = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(pointSize: 15))
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        initUI()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    private func initUI() {
+        // UIButton
+        deleteButton.setImage(BUTTON_IMAGE, for: .normal)
+    }
+    
+    func setData(name: String, index: Int) {
+        nameLabel.text = name
+        if index == 0 {
+            topConst.constant = PARTIAL_TOP_CONST
+        }
+    }
 }
