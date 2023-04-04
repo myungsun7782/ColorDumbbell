65c9380fded2e1e78431954b2859a0b75d0f9b4b
myungsun <audtjs9987@naver.com>
2023-04-04 20:54:26 +0900

    chore: add enum ExerciseType

---
+++ ColorDumbbell/Enums/ExerciseType.swift
@@ -0,0 +1,13 @@
+//
+//  ExerciseType.swift
+//  ColorDumbbell
+//
+//  Created by myungsun on 2023/04/04.
+//
+
+import Foundation
+
+enum ExerciseType: String  {
+    case basis = "basis"
+    case custom = "custom"
+}
