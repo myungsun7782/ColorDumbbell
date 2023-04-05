b73050fe448455571d23f3d48c07583ee0ca782e
myungsun <audtjs9987@naver.com>
2023-04-05 20:14:27 +0900

    change struct to class type in ExerciseJournal Model

--- ColorDumbbell/Models/ExerciseJournal.swift
+++ ColorDumbbell/Models/ExerciseJournal.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-struct ExerciseJournal {
+class ExerciseJournal {
     var id: String
     var title: String
     var registerDate: Date
@@ -16,5 +16,56 @@ struct ExerciseJournal {
     var totalExerciseTime: Int
     var photoIdArray: [String]?
     var exerciseArray: [Exercise]
-    var groupedExerciseArray: [[Exercise]]?
+    var groupedExerciseArray: [[Exercise]]
+    var registeredDateString: String
+    
+    init(id: String, title: String, registerDate: Date, startTime: Date, endTime: Date, totalExerciseTime: Int, photoIdArray: [String]? = nil, exerciseArray: [Exercise]) {
+        self.id = id
+        self.title = title
+        self.registerDate = registerDate
+        self.startTime = startTime
+        self.endTime = endTime
+        self.totalExerciseTime = totalExerciseTime
+        self.photoIdArray = photoIdArray
+        self.exerciseArray = exerciseArray
+        self.groupedExerciseArray = []
+        self.registeredDateString = TimeManager.shared.dateToString(date: startTime, options: [.year, .month, .day])
+        
+        exerciseArray.forEach { exercise in
+            if isDivisionConstains(area: exercise.area) {
+                addExerciseToDivision(exercise: exercise)
+            } else {
+                groupedExerciseArray.append([exercise])
+            }
+        }
+    }
+    
+    func divisionExerciseArray(exerciseArray: [Exercise]) {
+        self.groupedExerciseArray = []
+        exerciseArray.forEach { exercise in
+            if isDivisionConstains(area: exercise.area) {
+                addExerciseToDivision(exercise: exercise)
+            } else {
+                groupedExerciseArray.append([exercise])
+            }
+        }
+    }
+    
+    private func isDivisionConstains(area: ExerciseArea.RawValue) -> Bool {
+        for division in groupedExerciseArray {
+            if division[0].area == area {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    private func addExerciseToDivision(exercise: Exercise) {
+        for (idx, groupedExercise) in groupedExerciseArray.enumerated() {
+            if groupedExercise[0].area == exercise.area {
+                groupedExerciseArray[idx].append(exercise)
+            }
+        }
+    }
 }
