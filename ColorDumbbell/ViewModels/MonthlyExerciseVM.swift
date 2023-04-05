3bd4d509fc2ee9072c94f2b5152d2445cab3d34b
myungsun <audtjs9987@naver.com>
2023-04-05 19:00:37 +0900

    chore: add properties in MonthlyExercise view model

--- ColorDumbbell/ViewModels/MonthlyExerciseVM.swift
+++ ColorDumbbell/ViewModels/MonthlyExerciseVM.swift
@@ -21,6 +21,9 @@ class MonthlyExerciseVM {
     // Variable
     var month: String!
     var exerciseJournalArray: [ExerciseJournal] = Array<ExerciseJournal>()
+    var modifiedExerciseJournal: ExerciseJournal?
+    var deletedExerciseJournal: ExerciseJournal?
+    var delegate: ExerciseJournalDelegate?
     
     // Constants
     
@@ -34,55 +37,5 @@ class MonthlyExerciseVM {
     
     init() {
 
-    }
-    
-    func getExerciseAreaCount(selectedJournal: ExerciseJournal) -> Int {
-        var count = 0
-        var exerciseAreaDictionary: [String: Int] = [ExerciseArea.back.rawValue: 0 , ExerciseArea.chest.rawValue: 0 , ExerciseArea.shoulder.rawValue: 0, ExerciseArea.leg.rawValue: 0, ExerciseArea.arm.rawValue: 0, ExerciseArea.abs.rawValue: 0]
-        
-        for exercise in selectedJournal.exerciseArray {
-            exerciseAreaDictionary[exercise.area]! += 1
-        }
-        
-        for (_, value) in exerciseAreaDictionary {
-            if value != 0 {
-                count += 1
-            }
-        }
-        
-        return count
-    }
-    
-    func makeGroupExerciseArray() {
-        for (idx, journal) in exerciseJournalArray.enumerated() {
-            var groupArray: [[Exercise]] = [[], [], [], [], [], []]
-            var groupedExerciseArray: [[Exercise]] = []
-            
-            for exercise in journal.exerciseArray {
-                switch exercise.area {
-                case ExerciseArea.back.rawValue:
-                    groupArray[0].append(exercise)
-                case ExerciseArea.chest.rawValue:
-                    groupArray[1].append(exercise)
-                case ExerciseArea.shoulder.rawValue:
-                    groupArray[2].append(exercise)
-                case ExerciseArea.leg.rawValue:
-                    groupArray[3].append(exercise)
-                case ExerciseArea.arm.rawValue:
-                    groupArray[4].append(exercise)
-                case ExerciseArea.abs.rawValue:
-                    groupArray[5].append(exercise)
-                default:
-                    break
-                }
-            }
-            
-            for group in groupArray {
-                if !group.isEmpty {
-                    groupedExerciseArray.append(group)
-                }
-            }
-            exerciseJournalArray[idx].groupedExerciseArray = groupedExerciseArray
-        }
     }
 }
