a947004c448699e9355aefd3b05b3c3f67a59e6d
myungsun <audtjs9987@naver.com>
2023-04-15 21:42:38 +0900

    chore: add enum WeekDay

--- ColorDumbbell/Enums/WeekDay.swift
+++ ColorDumbbell/Enums/WeekDay.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum WeekDay: String {
+    case mon = "월"
+    case tue = "화"
+    case wed = "수"
+    case thu = "목"
+    case fri = "금"
+    case sat = "토"
+    case sun = "일"
+}
