d31703602a3910b482e99710c0587a50b9ce6269
myungsun <audtjs9987@naver.com>
2023-04-15 21:49:19 +0900

    feat: develop RoutineSelectionCell

--- ColorDumbbell/Cells/UITableViewCell/RoutineSelectionCell.swift
+++ ColorDumbbell/Cells/UITableViewCell/RoutineSelectionCell.swift
@@ -6,18 +6,81 @@
 //
 
 import UIKit
+import RxSwift
 
 class RoutineSelectionCell: UITableViewCell {
-
+    // UIStackView
+    @IBOutlet weak var containerStackView: UIStackView!
+    @IBOutlet weak var lineStackView: UIStackView!
+    
+    // UILabel
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    // NSLayoutConstraint
+    @IBOutlet weak var topConst: NSLayoutConstraint!
+    @IBOutlet weak var bottomConst: NSLayoutConstraint!
+    
+    // RxSwift
+    var disposeBag = DisposeBag()
+    
+    // Variables
+    var isSingleData: Bool?
+    var index: Int?
+    var routineArray: [Routine]?
+    
+    // Constants
+    let STACK_VIEW_CORNER_RADIUS: CGFloat = 7
+    let NORMAL_CONSTARINT: CGFloat = 11
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
     }
     
+    override func setNeedsLayout() {
+        if let isSingleData = isSingleData, let index = index, let routineArray = routineArray {
+            if isSingleData {
+                containerStackView.layer.cornerRadius = STACK_VIEW_CORNER_RADIUS
+            } else if !isSingleData && index != routineArray.count - 1 && index != .zero {
+                containerStackView.layer.cornerRadius = .zero
+            } else if !isSingleData && index == .zero {
+                containerStackView.layer.cornerRadius = .zero
+                containerStackView.roundCorners(corners: [.topLeft, .topRight], radius: STACK_VIEW_CORNER_RADIUS)
+            }else {
+                containerStackView.layer.cornerRadius = .zero
+                containerStackView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: STACK_VIEW_CORNER_RADIUS)
+            }
+            self.layoutIfNeeded()
+        }
+    }
+    
+    func setData(index: Int, lastIndex: Int, isSingleData: Bool, name: String, routineArray: [Routine]) {
+        self.isSingleData = isSingleData
+        self.routineArray = routineArray
+        self.index = index
+        nameLabel.text = name
+    
+        if index == .zero && index == lastIndex {
+            topConst.constant = NORMAL_CONSTARINT
+            bottomConst.constant = NORMAL_CONSTARINT
+            lineStackView.isHidden = true
+        } else if index == .zero && index != lastIndex {
+            topConst.constant = NORMAL_CONSTARINT
+            bottomConst.constant = .zero
+            lineStackView.isHidden = false
+        } else if index != .zero && index == lastIndex {
+            topConst.constant = .zero
+            bottomConst.constant = NORMAL_CONSTARINT
+            lineStackView.isHidden = true
+        } else {
+            topConst.constant = .zero
+            bottomConst.constant = .zero
+            lineStackView.isHidden = false
+        }
+    }
 }
