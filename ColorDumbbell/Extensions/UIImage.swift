7408361692147b15ccb4da535caabad89c708b48
myungsun <audtjs9987@naver.com>
2023-04-15 21:41:00 +0900

    add UIImage extension

--- ColorDumbbell/Extensions/UIImage.swift
+++ ColorDumbbell/Extensions/UIImage.swift
@@ -5,4 +5,26 @@
 //  Created by myungsun on 2023/04/15.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    public convenience init?(bounds: CGRect, colors: [UIColor], orientation: GradientOrientation = .horizontal) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map({ $0.cgColor })
+        gradientLayer.cornerRadius = 6
+        
+        if orientation == .horizontal {
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5);
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5);
+        }
+        
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
