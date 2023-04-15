8aa1ba0c2e1bab6d4a22275a573e716c129690f2
myungsun <audtjs9987@naver.com>
2023-04-15 22:08:54 +0900

    feat: add methods in AddExercise view model

--- ColorDumbbell/ViewModels/AddExerciseVM.swift
+++ ColorDumbbell/ViewModels/AddExerciseVM.swift
@@ -24,11 +24,13 @@ class AddExerciseVM {
     var exerciseDelegate: ExerciseDelegate?
     var section: Int!
     var row: Int!
+    var totalExerciseArray: [[Exercise]] = ExerciseManager.shared.totalExerciseArray
     
     // Constants
     let MINIMUM_LENGTH: Int = 2
     let MAXIMUM_LENGTH: Int = 6
     let exerciseAreaArray: [ExerciseArea] = [.back, .chest, .shoulder, .leg, .arm, .abs]
+    let DUPLICATION_TEXT: String = "해당 운동은 이미 존재하는 운동입니다."
     
     struct Input {
         var exerciseName = BehaviorSubject<String>(value: "")
@@ -36,20 +38,28 @@ class AddExerciseVM {
     
     struct Output {
         var saveButtonValidation = PublishSubject<Bool>()
+        var isDuplicateExerciseName = PublishSubject<Bool>()
     }
     
     init() {
         input.exerciseName.subscribe(onNext: { exerciseName in
             self.output.saveButtonValidation.onNext(self.validateSaveButton(exerciseName: exerciseName))
+            self.output.isDuplicateExerciseName.onNext(self.isDuplicateExerciseName(exerciseName: exerciseName))
         })
         .disposed(by: disposeBag)
     }
     
     private func validateSaveButton(exerciseName: String) -> Bool {
-        if exerciseName.count >= 1 {
+        if exerciseName.count >= 1 && !isDuplicateExerciseName(exerciseName: exerciseName){
             return true
         }
         return false
     }
-
+    
+    private func isDuplicateExerciseName(exerciseName: String) -> Bool {
+        let trimmedArray = totalExerciseArray.map { $0.map { $0.name.replacingOccurrences(of: " ", with: "") } }
+        let trimmedExerciseName = exerciseName.replacingOccurrences(of: " ", with: "")
+        
+        return trimmedArray.flatMap({ $0 }).contains(where: { $0 == trimmedExerciseName })
+    }
 }
