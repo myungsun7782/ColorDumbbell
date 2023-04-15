dd6d368d21145091fe4dacdf21ba5afe44964244
myungsun <audtjs9987@naver.com>
2023-04-15 21:55:54 +0900

    refactor: clean up code associated with tableView

--- ColorDumbbell/ViewControllers/ExerciseSelectionVC.swift
+++ ColorDumbbell/ViewControllers/ExerciseSelectionVC.swift
@@ -89,6 +89,7 @@ class ExerciseSelectionVC: UIViewController {
         selectionTableView.register(UINib(nibName: Cell.exerciseTitleCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseTitleCell)
         selectionTableView.register(UINib(nibName: Cell.exerciseContentCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseContentCell)
         selectionTableView.register(UINib(nibName: Cell.exerciseRegisterCell, bundle: nil), forCellReuseIdentifier: Cell.exerciseRegisterCell)
+        selectionTableView.register(UINib(nibName: Cell.routineSelectionCell, bundle: nil), forCellReuseIdentifier: Cell.routineSelectionCell)
     }
     
     private func configureButton() {
@@ -116,6 +117,7 @@ class ExerciseSelectionVC: UIViewController {
         addExerciseVC.viewModel.exercise = exercise
         addExerciseVC.viewModel.section = section
         addExerciseVC.viewModel.row = row
+        addExerciseVC.viewModel.totalExerciseArray = viewModel.totalExerciseArray
         addExerciseVC.viewModel.exerciseDelegate = self
         addExerciseVC.modalPresentationStyle = .overCurrentContext
         addExerciseVC.modalTransitionStyle = .coverVertical
@@ -126,157 +128,190 @@ class ExerciseSelectionVC: UIViewController {
 
 extension ExerciseSelectionVC: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return NUMBER_OF_SECTION
+        return viewModel.routineArray.isEmpty ? viewModel.totalExerciseArray.count : viewModel.totalExerciseArray.count + 1
     }
-    
+  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[0].count + 2 : viewModel.totalExerciseArray[0].count + 1
-        } else if section == 1 {
-            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[1].count + 2 : viewModel.totalExerciseArray[1].count + 1
-        } else if section == 2 {
-            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[2].count + 2 : viewModel.totalExerciseArray[2].count + 1
-        } else if section == 3 {
-            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[3].count + 2 : viewModel.totalExerciseArray[3].count + 1
-        } else if section == 4 {
-            return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[4].count + 2 : viewModel.totalExerciseArray[4].count + 1
+        if viewModel.routineArray.isEmpty {
+            let totalExerciseCount = viewModel.totalExerciseArray[section].count
+            return viewModel.isEditorModeOn ? totalExerciseCount+2 : totalExerciseCount+1
+        } else {
+            if section == viewModel.totalExerciseArray.count {
+                return viewModel.routineArray.count
+            } else {
+                let totalExerciseCount = viewModel.totalExerciseArray[section].count
+                return viewModel.isEditorModeOn ? totalExerciseCount+2 : totalExerciseCount+1
+            }
         }
-        
-        return viewModel.isEditorModeOn ? viewModel.totalExerciseArray[5].count + 2 : viewModel.totalExerciseArray[5].count + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 || indexPath.section == 1 || indexPath.section == 2 || indexPath.section == 3 || indexPath.section == 4 || indexPath.section == 5 {
+        if indexPath.section == viewModel.totalExerciseArray.count {
+            if viewModel.routineArray.isEmpty { return UITableViewCell() }
+            return getRoutineSelectionCell(tableView: tableView, indexPath: indexPath)
+        } else {
+            if viewModel.totalExerciseArray[indexPath.section].isEmpty { return UITableViewCell() }
             if indexPath.row == 0 {
-                let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseTitleCell) as! ExerciseTitleCell
-                cell.setData(index: indexPath.section, isEditorModeOn: viewModel.isEditorModeOn)
-                
-                cell.editButton.rx.tap
-                    .subscribe(onNext: { _ in
-                        self.viewModel.isEditorModeOn = !self.viewModel.isEditorModeOn
-                        DispatchQueue.main.async {
-                            self.selectionTableView.reloadData()
-                            self.view.layoutIfNeeded()
-                        }
-                    })
-                    .disposed(by: cell.disposeBag)
-                
-                return cell
-            }
-            if !viewModel.isEditorModeOn {
-                if indexPath.row != 0 {
-                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
-                    
-                    cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name, isEditorModeOn: false, index: indexPath.row, exerciseArray: viewModel.totalExerciseArray[indexPath.section])
-                    cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
-                    cell.containerStackView.rx.tapGesture()
-                        .when(.recognized)
-                        .subscribe(onNext: { _ in
-                            self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked = !self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked
-                            DispatchQueue.main.async {
-                                self.selectionTableView.reloadData()
-                            }
-                            self.configureButton()
-                        })
-                        .disposed(by: cell.disposeBag)
-                    
-                    return cell
-                }
+                return getExerciseTitleCell(tableView: tableView, indexPath: indexPath)
             } else {
-                if indexPath.row == viewModel.totalExerciseArray[indexPath.section].count + 1 {
-                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseRegisterCell) as! ExerciseRegisterCell
-                    
-                    cell.setData(isEditorModeOn: viewModel.isEditorModeOn)
-                    cell.containerStackView.rx.tapGesture()
-                        .when(.recognized)
-                        .subscribe(onNext: { _ in
-                            self.presentAddExerciseVC(editorMode: .new,
-                                                      section: indexPath.section,
-                                                      row: indexPath.row-1)
-                        })
-                        .disposed(by: cell.disposeBag)
-                    
-                    return cell
+                if viewModel.isEditorModeOn && indexPath.row == viewModel.totalExerciseArray[indexPath.section].count+1 {
+                    return getExerciseRegisterCell(tableView: tableView, indexPath: indexPath)
                 } else {
-                    let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
-                    
-                    cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name,
-                                 isEditorModeOn: true,
-                                 index: indexPath.row,
-                                 exerciseArray: viewModel.totalExerciseArray[indexPath.section])
-                    cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
-                    cell.containerStackView.rx.tapGesture()
-                        .when(.recognized)
-                        .subscribe(onNext: { _ in
-                            self.presentAddExerciseVC(editorMode: .edit,
-                                                      section: indexPath.section,
-                                                      row: indexPath.row-1,
-                                                      exercise: self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1])
-                            
-                            DispatchQueue.main.async {
-                                self.selectionTableView.reloadData()
-                            }
-                            self.configureButton()
-                        })
-                        .disposed(by: cell.disposeBag)
-                    
-                    cell.deleteButton.rx.tap
-                        .subscribe(onNext: { _ in
-                            AlertManager.shared.presentTwoButtonAlert(title: "운동 삭제", message: "정말로 해당 운동을 삭제하시겠습니까?", buttonTitle: "확인", style: .alert) {
-                                // MARK: - Cloud Firestore에서도 삭제하기
-                                let selectedExercise = self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1]
-                                if selectedExercise.type == ExerciseType.basis.rawValue {
-                                    FirebaseManager.shared.deleteDefaultExercise(exercise: selectedExercise) { isSuccess in
-                                        if isSuccess {
-                                            self.viewModel.totalExerciseArray[indexPath.section].remove(at: indexPath.row-1)
-                                            DispatchQueue.main.async {
-                                                self.selectionTableView.reloadData()
-                                            }
-                                        }
-                                    }
-                                } else if selectedExercise.type == ExerciseType.custom.rawValue {
-                                    FirebaseManager.shared.deleteCustomExercise(exercise: selectedExercise) { isSuccess in
-                                        if isSuccess {
-                                            self.viewModel.totalExerciseArray[indexPath.section].remove(at: indexPath.row-1)
-                                            DispatchQueue.main.async {
-                                                self.selectionTableView.reloadData()
-                                            }
-                                        }
-                                    }
-                                }
-                            } completionHandler: { alert in
-                                self.present(alert, animated: true)
-                            }
-                        })
-                        .disposed(by: cell.disposeBag)
-                    
-                    return cell
+                    return getExerciseContentCell(tableView: tableView, indexPath: indexPath, isEditorMode: viewModel.isEditorModeOn)
                 }
             }
         }
-        return UITableViewCell()
+    }
+    
+    func getRoutineSelectionCell(tableView: UITableView, indexPath: IndexPath) -> RoutineSelectionCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cell.routineSelectionCell) as! RoutineSelectionCell
+        let isSingleData = viewModel.routineArray.count == 1 ? true : false
+
+        cell.setData(index: indexPath.row,
+                     lastIndex: viewModel.routineArray.count - 1,
+                     isSingleData: isSingleData,
+                     name: viewModel.routineArray[indexPath.row].name,
+                     routineArray: viewModel.routineArray)
+        cell.containerStackView.rx.tapGesture()
+            .when(.recognized)
+            .subscribe(onNext: { _ in
+                self.viewModel.routineDelegate?.transferData(section: nil,
+                                                             routine: self.viewModel.routineArray[indexPath.row],
+                                                             editorMode: .new)
+                self.dismiss(animated: true)
+            })
+            .disposed(by: cell.disposeBag)
+
+        return cell
+    }
+    
+    func getExerciseTitleCell(tableView: UITableView, indexPath: IndexPath) -> ExerciseTitleCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseTitleCell) as! ExerciseTitleCell
+        cell.setData(index: indexPath.section, isEditorModeOn: viewModel.isEditorModeOn)
+        
+        cell.editButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.viewModel.isEditorModeOn = !self.viewModel.isEditorModeOn
+                DispatchQueue.main.async {
+                    self.selectionTableView.reloadData()
+                    self.view.layoutIfNeeded()
+                }
+            })
+            .disposed(by: cell.disposeBag)
+        
+        return cell
+    }
+    
+    func getExerciseRegisterCell(tableView: UITableView, indexPath: IndexPath) -> ExerciseRegisterCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseRegisterCell) as! ExerciseRegisterCell
+        
+        cell.setData(isEditorModeOn: viewModel.isEditorModeOn)
+        cell.containerStackView.rx.tapGesture()
+            .when(.recognized)
+            .subscribe(onNext: { _ in
+                self.presentAddExerciseVC(editorMode: .new,
+                                          section: indexPath.section,
+                                          row: indexPath.row-1)
+            })
+            .disposed(by: cell.disposeBag)
+        
+        return cell
+    }
+    
+    func getExerciseContentCell(tableView: UITableView, indexPath: IndexPath, isEditorMode: Bool) -> ExerciseContentCell {
+        if isEditorMode {
+            let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
+            
+            cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name,
+                         isEditorModeOn: true,
+                         index: indexPath.row,
+                         exerciseArray: viewModel.totalExerciseArray[indexPath.section])
+            cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
+            cell.containerStackView.rx.tapGesture()
+                .when(.recognized)
+                .subscribe(onNext: { _ in
+                    self.presentAddExerciseVC(editorMode: .edit,
+                                              section: indexPath.section,
+                                              row: indexPath.row-1,
+                                              exercise: self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1])
+                    
+                    DispatchQueue.main.async {
+                        self.selectionTableView.reloadData()
+                    }
+                    self.configureButton()
+                })
+                .disposed(by: cell.disposeBag)
+            
+            cell.deleteButton.rx.tap
+                .subscribe(onNext: { _ in
+                    AlertManager.shared.presentTwoButtonAlert(title: "운동 삭제", message: "정말로 해당 운동을 삭제하시겠습니까?", buttonTitle: "확인", style: .alert) {
+                        // MARK: - Cloud Firestore에서도 삭제하기
+                        let selectedExercise = self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1]
+                        ExerciseManager.shared.figureExercise(exerciseObj: selectedExercise, editorMode: .delete)
+                        if selectedExercise.type == ExerciseType.basis.rawValue {
+                            FirebaseManager.shared.deleteDefaultExercise(exercise: selectedExercise) { isSuccess in
+                                if isSuccess {
+                                    self.viewModel.totalExerciseArray[indexPath.section].remove(at: indexPath.row-1)
+                                    DispatchQueue.main.async {
+                                        self.selectionTableView.reloadData()
+                                    }
+                                }
+                            }
+                        } else if selectedExercise.type == ExerciseType.custom.rawValue {
+                            FirebaseManager.shared.deleteCustomExercise(exercise: selectedExercise) { isSuccess in
+                                if isSuccess {
+                                    self.viewModel.totalExerciseArray[indexPath.section].remove(at: indexPath.row-1)
+                                    DispatchQueue.main.async {
+                                        self.selectionTableView.reloadData()
+                                    }
+                                }
+                            }
+                        }
+                    } completionHandler: { alert in
+                        self.present(alert, animated: true)
+                    }
+                })
+                .disposed(by: cell.disposeBag)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: Cell.exerciseContentCell) as! ExerciseContentCell
+            
+            if !viewModel.totalExerciseArray[indexPath.section].isEmpty {
+                cell.setData(exerciseName: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].name, isEditorModeOn: false, index: indexPath.row, exerciseArray: viewModel.totalExerciseArray[indexPath.section])
+                cell.setCheckButtonConfiguration(isClicked: viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked)
+                cell.containerStackView.rx.tapGesture()
+                    .when(.recognized)
+                    .subscribe(onNext: { _ in
+                        self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked = !self.viewModel.totalExerciseArray[indexPath.section][indexPath.row-1].isChecked
+                        DispatchQueue.main.async {
+                            self.selectionTableView.reloadData()
+                        }
+                        self.configureButton()
+                    })
+                    .disposed(by: cell.disposeBag)
+            }
+            
+            return cell
+        }
     }
 }
 
 extension ExerciseSelectionVC: UIAdaptivePresentationControllerDelegate {
     func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
-        presentActionSheetAlert()
         return false
     }
 }
 
 extension ExerciseSelectionVC: ExerciseDelegate {
     func selectExercises(exerciseArray: [Exercise]) {}
-    func manageExercise(section: Int, row: Int, exerciseName: String, editorMode: EditorMode) {
+    func manageExercise(section: Int, row: Int, exercise: Exercise, editorMode: EditorMode) {
         if editorMode == .new {
-            let exercise: Exercise = Exercise(name: exerciseName,
-                                              area: viewModel.EXERCISE_AREA_ARRAY[section].rawValue,
-                                              quantity: [ExerciseQuantity](),
-                                              id: UUID().uuidString,
-                                              type: ExerciseType.custom.rawValue)
             viewModel.totalExerciseArray[section].append(exercise)
+            ExerciseManager.shared.figureExercise(exerciseObj: exercise, editorMode: .new)
         } else if editorMode == .edit {
-            viewModel.totalExerciseArray[section][row].name = exerciseName
+            ExerciseManager.shared.figureExercise(exerciseObj: viewModel.totalExerciseArray[section][row], editorMode: .edit)
+            viewModel.totalExerciseArray[section][row].name = exercise.name
         }
         selectionTableView.reloadData()
     }
